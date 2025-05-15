4bc97aaa9ebf5c377dd13a8852edb856e8d6286c
julioedi <131900135+julioedward@users.noreply.github.com>
2025-05-15 04:56:07 -0700

    fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import All, { AllScreen } from '@root//screens/All';
 import Moon from './components/Moon';
 import { __ } from './utilities/Lang';
 
-const continents = ["africa", "america","americas", "asia", "europe", "oceania",""];
+const continents = ["africa", "america", "americas", "asia", "europe", "oceania", ""];
 class App extends Component {
   Layout = () => {
     return (
@@ -15,9 +15,10 @@ class App extends Component {
           <h1>{__("Where in the world")}</h1>
           <div
             className='theme_toggle'
-            onClick={() =>{
-              document.body.classList.toggle("dark_theme");
-              const is_dark = document.body.classList.contains("dark_theme");
+            onClick={() => {
+              const el = document.head.parentElement;
+              el?.classList.toggle("dark_theme");
+              const is_dark = el?.classList.contains("dark_theme");
               window.localStorage.setItem("theme", is_dark ? "dark" : "light");
             }}
           >
@@ -43,7 +44,7 @@ class App extends Component {
                 <Route path={item} element={<All path={item} />} key={index} />
               ))
             }
-            <Route path={"*"} element={<All path={"*"} />}/>
+            <Route path={"*"} element={<All path={"*"} />} />
           </Route>
         </Routes>
       </BrowserRouter>
